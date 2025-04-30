44b4e9cae2d889bc819752837a4a7b194412bb94
Patrick Styll <patrick.styll@tuwien.ac.at>
2025-04-30 20:00:53 +0200

    add a trainer for encoder-based transformer models

--- src/medaidml/__init__.py
+++ src/medaidml/__init__.py
@@ -5,3 +5,13 @@ ROOT_DIR = MODULE_DIR.parent.parent
 DATA_DIR = ROOT_DIR / "data"
 DATA_TEST_JSON = DATA_DIR / "dataset_test.json"
 DATA_TRAIN_JSON = DATA_DIR / "dataset_train.json"
+
+ID_TO_LABEL = {
+    0: "Human",
+    1: "AI"
+}
+
+LABEL_TO_ID = {
+    "Human": 0,
+    "AI": 1
+}
